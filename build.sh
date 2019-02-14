7a3c043fc56e0805b7dc3dcfdb1bcb2e75a2e035
guilhem <guilhem.marchand@gmail.com>
2019-02-14 00:05:41 +0000

    Fix Ubuntu 18 issue

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PWD=`pwd`
 app=`basename $PWD`
 version=`grep 'version =' ${app}/default/app.conf | awk '{print $3}' | sed 's/\.//g'`
 
-tar -czf ${app}_${version}.tgz ${app} --exclude=TA-nmon/local --exclude=TA-nmon/metadata/local.meta --exclude=TA-nmon/bin/linux
+tar -czf ${app}_${version}.tgz --exclude=TA-nmon/local --exclude=TA-nmon/metadata/local.meta --exclude=TA-nmon/bin/linux ${app}
 echo "Wrote: ${app}_${version}.tgz"
 
 exit 0
